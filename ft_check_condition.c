da79c6dcf17c56b34001e9557d17672435a9a7a9
Naahiki <112497828+Naahiki@users.noreply.github.com>
2022-10-06 17:57:41 +0200

    Add files via upload

---
+++ ft_check_condition.c
@@ -0,0 +1,23 @@
+#include "ft_printf.h"
+
+int	ft_check_condition(va_list arg, char c)
+{
+	int	cont;
+
+	cont = 0;
+	if (c == 'c')
+		cont = ft_putchar(va_arg(arg, int));
+	if (c == 's')
+		cont = ft_putstr(va_arg(arg, char *));
+	if (c == '%')
+		cont = ft_putchar('%');
+	if (c == 'i' || c == 'd')
+		cont = ft_putnbr(va_arg(arg, int));
+	if (c == 'u')
+		cont = ft_put_unsignednb(va_arg(arg, unsigned int));
+	if (c == 'x' || c == 'X')
+		cont = ft_puthexa(va_arg(arg, unsigned int), c);
+	if (c == 'p')
+		cont = ft_putstr("0x") + ft_puthexa(va_arg(arg, size_t), c);
+	return (cont);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -1,18 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nrodrigu <nrodrigu@student.42urduliz.co    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/30 15:16:23 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/10/03 16:44:47 by nrodrigu         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char const *s, ...)
+int	ft_printf(const char *s, ...)
 {
+	va_list	arg;
+	int	i;
+	int	cont;
 
+	i = 0;
+	cont = 0;
+	va_start(arg, s);
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+		
+			cont = cont + ft_checkcondition(arg, s[i + 1]);
+			i++;
+		}
+		else  
+		{
+			cont = cont + ft_putchar(s[i]);
+				i++;
+		}
+	}
+	va_end(arg);
+	return(cont);
 }
