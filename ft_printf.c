0e1baa5003c0a99ed348c80fdddcfa0448c8a7ff
Naahiki <112497828+Naahiki@users.noreply.github.com>
2022-10-07 18:49:41 +0200

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,23 @@ int	ft_printf(const char *s, ...)
 	va_list	arg;
 	int	i;
 	int	cont;
-	int	k;
 
 	i = 0;
 	cont = 0;
 	va_start(arg, s);
 	while (s[i])
 	{
-		if (s[i] == '%')
+		if (s[i] != '%')
 		{
 		
-			cont = cont + ft_check_condition(arg, s[i + 1]);
-			i++; /* i = i+2*/
+			cont = ft_putchar(s[i]);
+			i++;
+			cont++;
 		}
 		else  
 		{
-			cont = cont + ft_putchar(s[i]);
-				i++;
-				cont++;
+			cont = cont + ft_check_condition(arg, s[i + 1]);
+				i = i + 2;
 		}
 	}
 	va_end(arg);
