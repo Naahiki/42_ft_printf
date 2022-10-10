99db45509487c099378abe578a85ea048dd5c825
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-10-10 11:10:59 +0200

    ft_printf.c modified

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 11:05:30 by nrodrigu          #+#    #+#             */
+/*   Updated: 2022/10/10 11:09:39 by nrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	arg;
-	int	i;
-	int	cont;
-	int	k;
+	int		i;
+	int		cont;
+	int		k;
 
 	i = 0;
 	cont = 0;
@@ -14,8 +25,7 @@ int	ft_printf(const char *s, ...)
 	while (s[i])
 	{
 		if (s[i] == '%')
-		{
-		
+		{	
 			cont = cont + ft_check_condition(arg, s[i + 1]);
 			i++; /* i = i+2*/
 		}
