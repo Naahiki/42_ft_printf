4f5d10ff7f6db0f3cfc0ce1a1b9c5a7417a61813
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-10-10 11:19:58 +0200

    Norminette

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 11:05:30 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/10/10 11:09:39 by nrodrigu         ###   ########.fr       */
+/*   Updated: 2022/10/10 11:14:34 by nrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,26 @@
 int	ft_printf(const char *s, ...)
 {
 	va_list	arg;
-<<<<<<< HEAD
 	int		i;
 	int		cont;
-	int		k;
-=======
-	int	i;
-	int	cont;
->>>>>>> fd7d05b57b24f3825d9843ee1b61b8ea04456866
 
 	i = 0;
 	cont = 0;
 	va_start(arg, s);
 	while (s[i])
 	{
-<<<<<<< HEAD
-		if (s[i] == '%')
-		{	
-			cont = cont + ft_check_condition(arg, s[i + 1]);
-			i++; /* i = i+2*/
-=======
 		if (s[i] != '%')
 		{
-		
 			cont = ft_putchar(s[i]);
 			i++;
 			cont++;
->>>>>>> fd7d05b57b24f3825d9843ee1b61b8ea04456866
 		}
-		else  
+		else
 		{
 			cont = cont + ft_check_condition(arg, s[i + 1]);
 				i = i + 2;
 		}
 	}
-	va_end(arg);
-	return(cont);
+	va_end (arg);
+	return (cont);
 }
--- ft_puthexa.c
+++ ft_puthexa.c
@@ -6,13 +6,14 @@
 /*   By: nrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:58:24 by nrodrigu          #+#    #+#             */
-/*   Updated: 2022/10/04 16:58:32 by nrodrigu         ###   ########.fr       */
+/*   Updated: 2022/10/10 11:19:26 by nrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /***************EJEMPLO CALCULO HEXADECIMAL********************/
 /* El numero 2619 decimal en hexadecimal es A3B ¿Como se calcula?*/
-/* Hexadecimal puede tener estos valores: 0123456789ABCDEF, donde A=10, B=11; C=12, D=13, E=14, F=15.
+/* Hexadecimal puede tener estos valores: 0123456789ABCDEF, donde 
+ * A=10, B=11; C=12, D=13, E=14, F=15.
  * Si nuestro numero esn = 2619 (decimal)
 2619 / 16 = 163 (2619 % 16 = módulo = 11, donde en hexadecimal 11 = B)
 163 / 16 = 10 (163 % 16 = módulo = 3)
@@ -30,17 +31,16 @@ int	ft_puthexa(size_t nb, char c)
 	if (c == 'x' || c == 'p')
 	{
 		if (nb >= 16)
-			i+=ft_puthexa(nb / 16, c);
-		i+=ft_putchar("0123456789abcdef"[nb % 16]);
+			i += ft_puthexa(nb / 16, c);
+		i += ft_putchar("0123456789abcdef"[nb % 16]);
 		return (i);
 	}
 	if (c == 'X')
 	{
 		if (nb >= 16)
-			i+=ft_puthexa(nb / 16, c);
-		i+=ft_putchar("0123456789ABCDEF"[nb % 16]);
+			i += ft_puthexa(nb / 16, c);
+		i += ft_putchar("0123456789ABCDEF"[nb % 16]);
 		return (i);
-
 	}
 	return (0);
 }
